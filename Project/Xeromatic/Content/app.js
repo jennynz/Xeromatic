dc3cdc2953c979d4d8ac1bc851b1d544a505e575
jennynz <jenny.s@hotmail.co.nz>
2016-04-09 15:12:24 +1200

    Add pinning functionality

--- Project/Xeromatic/Content/app.js
+++ Project/Xeromatic/Content/app.js
@@ -13,29 +13,29 @@ var Tweet = React.createClass({
 //React component that takes in some text as a property and displays it
 var Button = React.createClass({
     render: function() {
-        return <button className="btn btn-info pull-right">{this.props.label}</button>
+        return <button onClick={this.props.link} className="btn btn-info pull-right">{this.props.label}</button>
     }
 });
 
 //React component that makes a call to the API in the HomeController. If more than one tweet is returned, it displays a Tweet component for each.
 var App = React.createClass({
-	//React function that sets the initial state of the app (where changeable data is stored)
-	getInitialState: function() {
-	    return {
-	        recentTweets: [],
+    //React function that sets the initial state of the app (where changeable data is stored)
+    getInitialState: function() {
+        return {
+            recentTweets: [],
             pinnedTweets: []
-	    };
-	},
+        };
+    },
 
-	//React function that runs after the app first loads
-	componentDidMount: function() {
+    //React function that runs after the app first loads
+    componentDidMount: function() {
 	    
-	    var self = this;
+        var self = this;
 
-	    // Wait for both fetches to return before setting state.
+        // Wait for both fetches to return before setting state.
         var recentFetch = fetch('/recentTweets', {method: 'get'})
 			.then(function(response) {
-				return response.json();
+			    return response.json();
 			})
 
         var pinnedFetch = fetch('/pinnedTweets', {method: 'get'})
@@ -48,26 +48,57 @@ var App = React.createClass({
 			.then(function(data) {
 			    self.setState({
 			        recentTweets: data[0],
-                    pinnedTweets: data[1]
+			        pinnedTweets: data[1]
 			    });
 			})
 			.catch(function(error) {
 			    console.error('Error', error);
 			});
-	},
+    },
 
-	//React function that runs on first load and whenever the state is changed
-	render: function() {
-	    var pinnedTweets = (this.state.pinnedTweets.length > 0) ? this.state.pinnedTweets.map(function(tweet) {
+    //React function/method within App class that pins tweet
+    pin: function(tweet) {
+        var self = this;
+
+        // Add parameters to fetch
+        fetch('/pinTweet', {
+            method: 'post',
+            headers: new Headers({
+                'Content-Type' : 'application/json'
+            }),
+            // What we're actually sending to the API
+            body: JSON.stringify(tweet)
+        })
+		.then(function(response) {
+		    // Make a data array of current pinned tweets
+		    var data = self.state.pinnedTweets;
+
+            // Push the tweet that we want to pin to the data array
+		    data.push(tweet)
+
+            // Refresh the pinnedTweets array with newly updated data array which includes the newly pinned tweet
+		    self.setState({pinnedTweets: data});
+		})
+        .catch(function(error) {
+            console.error('Error', error);
+        });
+    },
+
+    //React function that runs on first load and whenever the state is changed
+    render: function() {
+
+        var self = this;
+
+        var pinnedTweets = (this.state.pinnedTweets.length > 0) ? this.state.pinnedTweets.map(function(tweet) {
             // For each item in pinnedTweets array, return a Tweet object
-			return <Tweet key={tweet.Id} text={tweet.Text} />
-			})
+            return <Tweet key={tweet.Id} text={tweet.Text} />
+            })
 			: null;
 
-		var recentTweets = (this.state.recentTweets.length > 0) ? this.state.recentTweets.map(function(tweet) {
-		    return (
+        var recentTweets = (this.state.recentTweets.length > 0) ? this.state.recentTweets.map(function(tweet) {
+            return (
                 <Tweet key={tweet.Id} text={tweet.Text}>
-                <Button label="Pin" />
+                    <Button link={function(){self.pin(tweet)}} label="Pin" />
                 </Tweet>    
             )
 		    })
@@ -75,8 +106,21 @@ var App = React.createClass({
 
 		return (
 			<div className="container">
-				<img src="/Content/UNYouthNZLogo.jpg" />
-				<div className="panel panel-default">
+                <table className="header-table"><tbody>
+                    <tr>
+                        <td><img src="/Content/UNYouthNZLogo.jpg" /></td>
+                        <td>
+                            <a className="nav-link" href="https://unyouth.org.nz/about">About</a>
+                            <a className="nav-link" href="https://unyouth.org.nz/regions">Regions</a>
+                            <a className="nav-link" href="https://unyouth.org.nz/events">Events</a>
+                            <a className="nav-link" href="https://unyouth.org.nz/alumni">Alumni</a>
+				            <a className="nav-link" href="https://blogs.unyouth.org.nz/">Blog</a>
+                            <a className="nav-link" href="https://unyouth.org.nz/get-involved">Join Us</a>
+                            <a className="nav-link" href="https://unyouth.org.nz/contact">Contact</a>
+                        </td>
+                    </tr>
+                </tbody></table>
+                <div className="panel panel-default">
 					<div className="panel-heading">
 						<h3 className="panel-title">Pinned Tweets</h3>
 					</div>
